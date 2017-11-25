56d544afeffb9a27dc94fc09285930837a5a55a9
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2017-11-25 09:49:55 +0200

    пример сглаживания

--- src/my/tanks/display/Display.java
+++ src/my/tanks/display/Display.java
@@ -2,6 +2,7 @@ package my.tanks.display;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 import java.util.Arrays;
@@ -16,12 +17,14 @@ public abstract class Display {
     private static Graphics bufferGraphics;
     private static int clearColor;
 
+    private static BufferStrategy bufferStrategy;
+
     //
     private static float delta = 0;
     //
 
 
-    public static void create(int width, int height, String title, int _clearColor){
+    public static void create(int width, int height, String title, int _clearColor, int numBuffers){
         if(created)return;
         window = new JFrame(title);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -43,6 +46,11 @@ public abstract class Display {
 
         clearColor = _clearColor;
 
+        content.createBufferStrategy(numBuffers);
+        bufferStrategy = content.getBufferStrategy();
+
+
+
         created = true;
     }
 
@@ -53,12 +61,22 @@ public abstract class Display {
     public static void render(){
         bufferGraphics.setColor(new Color(0xff0000ff));
         bufferGraphics.fillOval((int)( 350 + (Math.sin(delta) * 200)), 250, 100, 100);  //for demo of running..
-        delta += 0.02f;
+        delta += 0.02f;  /*движение круга*/
+
+        /*
+        Сглаживание: круг №2 размытый по краям. после отрисовки сглаживание отключается чтобы не распространялось на другие объекты
+        */
+
+        ((Graphics2D)bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        bufferGraphics.fillOval((int)( 550 + (Math.sin(delta) * 200)), 250, 100, 100);
+        ((Graphics2D)bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF); /*off*/
+
     }
 
     public static void swapBuffers(){
-        Graphics g = content.getGraphics();
+        Graphics g = bufferStrategy.getDrawGraphics();
         g.drawImage(buffer, 0, 0, null);
+        bufferStrategy.show();
 
     }
 
--- src/my/tanks/main/Main.java
+++ src/my/tanks/main/Main.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionEvent;
 
 public class Main {
     public static void main(String[] args) {
-        Display.create(800, 600, "Tanks", 0xffffcc00);
+        Display.create(800, 600, "Tanks", 0xffffcc00, 3);
         Timer t = new Timer(1000 / 60, new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
